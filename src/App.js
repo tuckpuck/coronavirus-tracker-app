2eec5231c417dd7c393def8163224d35f22ec87c
tuckpuck <tuckertriggs@gmail.com>
2020-05-02 09:10:08 -0600

    add App css module with some base styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <h1>App</h1>
         <Cards />
         <CountryPicker />
