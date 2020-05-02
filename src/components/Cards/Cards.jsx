19935fd23e2ab1e821fda346801d9e0f474d58ca
tuckpuck <tuckertriggs@gmail.com>
2020-05-02 13:10:08 -0600

    feed Cards with API data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,11 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -12,8 +16,8 @@ const Cards = (props) => {
             <Typography color="text-secondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="text-secondary">REAL DATE</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography color="text-secondary">{lastUpdate}</Typography>
             <Typography variant="body2">
               Number of active Covid-19 cases
             </Typography>
@@ -24,8 +28,8 @@ const Cards = (props) => {
             <Typography color="text-secondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="text-secondary">REAL DATE</Typography>
+            <Typography variant="h5">{recovered.value}</Typography>
+            <Typography color="text-secondary">{lastUpdate}</Typography>
             <Typography variant="body2">
               Number of recoveries from Covid-19
             </Typography>
@@ -36,8 +40,8 @@ const Cards = (props) => {
             <Typography color="text-secondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="text-secondary">REAL DATE</Typography>
+            <Typography variant="h5">{deaths.value}</Typography>
+            <Typography color="text-secondary">{lastUpdate}</Typography>
             <Typography variant="body2">
               Number of deaths from Covid-19
             </Typography>
