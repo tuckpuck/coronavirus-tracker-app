ac9e12080a8d8b4cd322b443dc52f1165b1485a2
tuckpuck <tuckertriggs@gmail.com>
2020-05-02 10:47:18 -0600

    make API folder with call to covid19.mathdro.id/api, call function inApp.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 export default class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
