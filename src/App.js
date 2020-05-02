43591131f7b0d1fd803b6857b5192261a9f41433
tuckpuck <tuckertriggs@gmail.com>
2020-05-02 09:05:31 -0600

    Create basic functional components, import all into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { Cards, Chart, CountryPicker } from "./components";
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
